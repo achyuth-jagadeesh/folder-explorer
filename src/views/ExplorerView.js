f36d6c56d00d6e5f47d4e073cacc920d77b966ab
Learn <happy.anicut@gmail.com>
2019-12-17 08:30:30 +0530

    Integrated with the actual service.

--- src/views/ExplorerView.js
+++ src/views/ExplorerView.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FolderView from "./FolderView";
+import axios from "axios";
 
 class ExplorerComponent extends React.Component {
   constructor() {
@@ -9,74 +10,27 @@ class ExplorerComponent extends React.Component {
       fileDetl: {
         totalFileSize: 0,
         totalFiles: 0
-      }
+      },
+      dataLoaded: false
     };
   }
 
   componentWillMount() {
-    var data = [
-      {
-        type: "folder",
-        name: "haptic",
-        children: [{ type: "file", name: "ram.mpeg", size: 45523 }]
-      },
-      {
-        type: "folder",
-        name: "bluetooth",
-        children: [
-          {
-            type: "folder",
-            name: "online",
-            children: [
-              { type: "folder", name: "online", children: [] },
-              {
-                type: "file",
-                name: "savings_account_applications_olive.png",
-                size: 37864
-              },
-              {
-                type: "folder",
-                name: "virtual",
-                children: [
-                  { type: "folder", name: "cross-platform", children: [] },
-                  { type: "file", name: "eyeballs.png", size: 75269 },
-                  {
-                    type: "file",
-                    name: "money_market_account.mp4v",
-                    size: 30246
-                  },
-                  { type: "folder", name: "mobile", children: [] }
-                ]
-              },
-              { type: "folder", name: "redundant", children: [] },
-              {
-                type: "file",
-                name: "east_caribbean_dollar_solution.m2v",
-                size: 41558
-              }
-            ]
-          },
-          {
-            type: "file",
-            name: "e_markets_web_services_best_of_breed.htm",
-            size: 64257
-          }
-        ]
-      }
-    ];
-    var fileDetl = {
-      totalFileSize: 0,
-      totalFiles: 0
-    };
-
-    this.addExpandFlag(data, fileDetl);
-    this.setState({ folders: data, fileDetl });
+    axios.get("https://chal-locdrmwqia.now.sh").then(response => {
+      var rootData=response.data.data;
+      var fileDetl = {
+        totalFileSize: 0,
+        totalFiles: 0
+      };
+      this.calcFilesSizeAndCount(rootData, fileDetl);
+      this.setState({ folders: rootData, fileDetl, dataLoaded: true });
+    });
   }
 
-  addExpandFlag(children, fileDetl) {
+  calcFilesSizeAndCount(children, fileDetl) {
     for (var i = 0; i < children.length; i++) {
       if (children[i].type == "folder") {
-        this.addExpandFlag(children[i].children, fileDetl);
+        this.calcFilesSizeAndCount(children[i].children, fileDetl);
       } else {
         fileDetl.totalFileSize += children[i].size;
         fileDetl.totalFiles++;
@@ -87,14 +41,26 @@ class ExplorerComponent extends React.Component {
   render() {
     return (
       <div>
-        <FolderView children={this.state.folders} />
-        <div className="files-detl" >
-          <div>Total Files : {this.state.fileDetl.totalFiles}</div>
+        {!this.state.dataLoaded ? (
           <div>
-            Total file size :{" "}
-            {Math.round(this.state.fileDetl.totalFileSize / 1024)} MB
+            <strong>Please wait content loading...</strong>
           </div>
-        </div>
+        ) : this.state.folders.length > 0 ? (
+          <div>
+            <FolderView children={this.state.folders} />
+            <div className="files-detl">
+              <div>Total Files : {this.state.fileDetl.totalFiles}</div>
+              <div>
+                Total file size :{" "}
+                {Math.round(this.state.fileDetl.totalFileSize / 1024)} MB
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <strong>Folders list is empty</strong>
+          </div>
+        )}
       </div>
     );
   }
